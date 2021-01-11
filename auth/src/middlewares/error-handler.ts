7a0c830a6b8acb2d3292b1cceaaedb209d4a7172
justin0979 <justinm0979@gmail.com>
2021-01-11 15:21:39 -0600

    fix typos

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,14 @@
-import { Request, Response, Next } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
-  next: Next
+  next: NextFunction
 ) => {
   console.log("Something went wrong", err);
 
   res.status(400).send({
-    message: "Something went wrong",
+    message: err.message,
   });
 };
