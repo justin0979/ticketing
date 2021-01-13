06aef9a8d8b61837778a67474be50e88cf314f6f
justin0979 <justinm0979@gmail.com>
2021-01-13 10:14:44 -0600

    add serializeUser() to error subclasses

--- auth/src/errors/request-validation-errors.ts
+++ auth/src/errors/request-validation-errors.ts
@@ -1,10 +1,18 @@
-import { ValidationError } from "express-validator"; // Use to describe the requirements for the errors assigned to RequestValidationError
+// Use to describe the requirements for the errors assigned to RequestValidationError
+import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super();
 
     // Only b/c we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }
